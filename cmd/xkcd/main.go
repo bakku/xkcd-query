ed9f9abf0fd92a29f069af0024dbbd83b111ac05
Christian Paling <christian.paling@googlemail.com>
2017-08-29 23:18:13 +0200

    Populate index when executing the tool

--- cmd/xkcd/main.go
+++ cmd/xkcd/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"os"
 	"flag"
-	"fmt"
+	"os"
+
+	"github.com/bakku/xkcd"
 )
 
 func main() {
-	var indexLocation = flag.String("index", os.Getenv("HOME") + "/.xkcd_index.json", "the index file")
+	var indexLocation = flag.String("index", os.Getenv("HOME")+"/.xkcd_index.json", "the index file")
 	flag.Parse()
 
-	fmt.Println(*indexLocation)
+	index := xkcd.NewIndex(indexLocation)
+	index.Populate()
 }
