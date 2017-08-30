9250e90927b0fc65bb011f52a8eb4df48b7bfc25
Christian Paling <christian.paling@googlemail.com>
2017-08-30 08:55:10 +0200

    Overwrite flag usage function to correctly explain the usage

--- cmd/xkcd/main.go
+++ cmd/xkcd/main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"flag"
 	"os"
+	"fmt"
 
 	"github.com/bakku/xkcd"
 )
+
+func init() {
+	// overwrite usage output
+	flag.Usage = func() {
+		fmt.Println("Usage:")
+		fmt.Println("\txkcd [flags] <command>\n")
+		fmt.Println("\tCommands:")
+		fmt.Println("\t\trefresh\trefresh the index file (might take a while)\n")
+		fmt.Println("\tFlags:")
+		fmt.Println("\t\t-index\tpath to the index file or where the index should be saved (Default: $HOME/.xkcd_index.json)")
+	}
+}
 
 func main() {
 	var indexLocation = flag.String("index", os.Getenv("HOME")+"/.xkcd_index.json", "the index file")
