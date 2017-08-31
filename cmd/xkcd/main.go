1a90a963dc7f924cfa1f36bfddd5f65bd3c7bfb4
Christian Paling <christian.paling@googlemail.com>
2017-08-31 17:48:29 +0200

    Add query command to executable

--- cmd/xkcd/main.go
+++ cmd/xkcd/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"os"
 	"fmt"
+	"os"
 
 	"github.com/bakku/xkcd"
 )
@@ -12,11 +12,12 @@ func init() {
 	// overwrite usage output
 	flag.Usage = func() {
 		fmt.Println("Usage:")
-		fmt.Println("\txkcd [flags] <command>\n")
+		fmt.Println("\txkcd [flags] <command> [optional arguments]\n")
 		fmt.Println("\tFlags:")
 		fmt.Println("\t\t-index\t\tpath to the index file or where the index should be saved (Default: $HOME/.xkcd_index.json)")
 		fmt.Println("\tCommands:")
 		fmt.Println("\t\trefresh\t\trefresh the index file (might take a while)\n")
+		fmt.Println("\t\tquery\t\tqueries the index file (expects the query string as arguments after the command)\n")
 	}
 }
 
@@ -24,7 +25,7 @@ func main() {
 	var indexLocation = flag.String("index", os.Getenv("HOME")+"/.xkcd_index.json", "the index file")
 	flag.Parse()
 
-	if flag.NArg() < 1 {
+	if flag.NArg() < 1 || (flag.Arg(0) == "query" && flag.NArg() == 1) {
 		flag.Usage()
 		return
 	}
@@ -32,9 +33,27 @@ func main() {
 	index := xkcd.NewIndex(indexLocation)
 
 	switch flag.Arg(0) {
-		case "refresh":
-			index.Refresh()
-		default:
-			flag.Usage()
+	case "refresh":
+		index.Refresh()
+	case "query":
+		comics, ok := index.Query(flag.Args()[1:])
+		if !ok {
+			return
+		}
+
+		printComics(comics)
+	default:
+		flag.Usage()
+	}
+}
+
+func printComics(comics []*xkcd.Comic) {
+	if len(comics) == 0 {
+		fmt.Println("No results.")
+		return
+	}
+
+	for i, comic := range comics {
+		fmt.Printf("%d)\tNumber: %d\tLink: %s\n", i, comic.Num, comic.Img)
 	}
 }
